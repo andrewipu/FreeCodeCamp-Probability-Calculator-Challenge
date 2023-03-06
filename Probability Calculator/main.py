cc223f0d4742cf37b75553cad542fa114c7a1c06
Andrew Ipu <ipuandrew@gmail.com>
2023-03-06 14:07:56 +0300

    Sorted out contents issue

--- Probability Calculator/main.py
+++ Probability Calculator/main.py
@@ -3,14 +3,16 @@ import prob_calculator
 from unittest import main
 
 prob_calculator.random.seed(95)
-hat = prob_calculator.Hat(blue=4, red=2, green=6)
-probability = prob_calculator.experiment(
-    hat=hat,
-    expected_balls={"blue": 2,
-                    "red": 1},
-    num_balls_drawn=4,
-    num_experiments=3000)
-print("Probability:", probability)
+#hat = prob_calculator.Hat(blue=4, red=2, green=6)
+hat = prob_calculator.Hat(red=5,blue=2)
+hat = print(hat.draw(2))
+#probability = prob_calculator.experiment(
+    #hat=hat,
+    #expected_balls={"blue": 2,
+                    #"red": 1},
+    #num_balls_drawn=4,
+    #num_experiments=3000)
+#print("Probability:", probability)
 
 # Run unit tests automatically
-main(module='test_module', exit=False)
+#main(module='test_module', exit=False)
--- Probability Calculator/prob_calculator.py
+++ Probability Calculator/prob_calculator.py
@@ -3,7 +3,34 @@ import random
 # Consider using the modules imported above.
 
 class Hat:
+    #constructor
+    def __init__(self, **urn):
+        self.urn = urn
+        self.contents = []
+
+        #loop to generate list of individual items for each ball
+        for k, v in urn.items():
+            i = 0
+            while i < v:
+                i += 1
+                self.contents.append(k)
+
+    def draw(self, num_draw):
+        #list to store randomly drawn balls
+        contents_rand = []
+
+        #loop to randomly sample from hat
+        for i in random.sample(list(self.contents),num_draw):
+            contents_rand.append(i)
+        self.contents = self.contents[:-num_draw]
+        if num_draw > len(self.contents):
+            self.contents = self.contents[:+num_draw]
+        else:
+            return contents_rand
 
 
-#def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+
+
+
+def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     pass
